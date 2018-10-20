d48ba47061cfe30bebfb635d21df7e8b8fc938aa
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2018-10-19 21:11:50 -0700

    Remove unused functions.

--- src/bytes.rs
+++ src/bytes.rs
@@ -10,14 +10,6 @@ pub fn low_order_byte(n: u16) -> u8 {
     (n & 0xff) as u8
 }
 
-pub fn rotate_right(n: u8) -> u8 {
-    ((n & 0x01) << 7) | (n >> 1)
-}
-
-pub fn rotate_left(n: u8) -> u8 {
-    ((n & 0x80) >> 7) | (n << 1)
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -44,23 +36,5 @@ mod tests {
         assert_eq!(low_order_byte(0x23), 0x23);
         assert_eq!(low_order_byte(0xa800), 0);
         assert_eq!(low_order_byte(0x753f), 0x3f);
-    }
-
-    #[test]
-    fn rotate_right_test() {
-        assert_eq!(rotate_right(0), 0);
-        assert_eq!(rotate_right(0x01), 0x80);
-        assert_eq!(rotate_right(0x80), 0x40);
-        assert_eq!(rotate_right(0x44), 0x22);
-        assert_eq!(rotate_right(0xff), 0xff);
-    }
-
-    #[test]
-    fn rotate_left_test() {
-        assert_eq!(rotate_left(0), 0);
-        assert_eq!(rotate_left(0x01), 0x02);
-        assert_eq!(rotate_left(0x80), 0x01);
-        assert_eq!(rotate_left(0x44), 0x88);
-        assert_eq!(rotate_left(0xff), 0xff);
     }
 }
